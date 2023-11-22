46e26d3f51ceb21bf4f45e8ca96529184cb6a2f5
yunusabdullaev <yunusabdullaev0707@gmail.com>
2023-11-22 16:35:57 +0500

    Yunus, 1-commit , project was created

---
+++ bin/untitled20.dart
@@ -0,0 +1,5 @@
+import 'package:untitled20/untitled20.dart' as untitled20;
+
+void main(List<String> arguments) {
+  print('Hello world: ${untitled20.calculate()}!');
+}
---
+++ test/untitled20_test.dart
@@ -0,0 +1,8 @@
+import 'package:untitled20/untitled20.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
